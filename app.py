eae5ff0d60d8b8bbdf75f0f9117a412d2c08e272
gustavoandraded3-beep <gustavo.andraded3@gmail.com>
2026-07-17 15:26:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -181,7 +181,7 @@ def prepare_dataframe(df):
     df['Completed Time Parsed'] = parse_date_column(df['Completed Time'])
     df['Last Updated Time Parsed'] = parse_date_column(df['Last Updated Time'])
     
-  df['Is Closed'] = (
+    df['Is Closed'] = (
     (df['Status Clean'].isin(CLOSED_STATUSES)) |          # Status no conjunto de fechados
     (df['Status Clean'].str.contains('cancel', na=False)) | # Qualquer status contendo "cancel"
     (df['ClosedDT'].notna())                               # Tem data de fechamento
